21c6a6d7d41f7c35a82eb452f567d4837b646c30
KarstenCollins <kbcollins17@mail.wou.edu>
2021-04-11 15:33:54 -0700

    Can add posts with login authentication

--- SubmitData/models.py
+++ SubmitData/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 from django.contrib.auth.models import User
 from django.urls import reverse
 
 
 #change fields here and then migrate when you put in a real db for Billable
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    #this is where the rel. is. Foreign key is the user
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255, default="")
+    account_number = models.CharField(max_length=255, default="")
+    statement_date = models.DateField(default=timezone.now)
+    due_date = models.DateField(default=datetime.date.today)
+    date_entered = models.DateTimeField(default=timezone.now)
+    amount = models.CharField(max_length=255, default="")
+    is_paid = models.BooleanField(default=0)
+    payment_method = models.CharField(max_length=50, default="")
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #this is where the rel. is. Foreign key is the user
 
     def __str__(self):
         return self.title
--- SubmitData/views.py
+++ SubmitData/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
@@ -16,15 +17,15 @@ class PostListView(ListView):
     model = Post #this is what will be queried from models.py
     template_name = 'SubmitData/home.html' #this changes the default template that django wants
     context_object_name = 'posts' #this is what you want the list to display
-    ordering = ['-date_posted']#newest to oldest ordering
+    ordering = ['-date_entered']#newest to oldest ordering
 
 
 class PostDetailView(DetailView):
     model = Post 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'company_name', 'account_number', 'statement_date', 'due_date', 'amount', 'payment_method', 'is_paid']
 
     def form_valid(self, form):
         form.instance.author = self.request.user #sets author to logged in user
