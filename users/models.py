1b9ac3e579cf846f4030f09f4f081fc1099ef85b
acourtright17 <72185346+acourtright17@users.noreply.github.com>
2021-04-20 16:55:02 -0700

    First and Last profile

--- users/models.py
+++ users/models.py
@@ -8,10 +8,12 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    first_name = models.CharField(max_length=30, default='')
+    last_name = models.CharField(max_length=30, default='')
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
+
     def save(self, *args, **kwargs):
         super(Profile,self).save(*args, **kwargs)
 
@@ -21,5 +23,3 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
-
