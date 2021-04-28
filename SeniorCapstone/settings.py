834c80f39a5004560113899ffed89e149b04b50c
KarstenCollins <kbcollins17@mail.wou.edu>
2021-04-27 17:32:50 -0700

    User can record "amount with non default value

--- SeniorCapstone/settings.py
+++ SeniorCapstone/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'BankInformation.apps.BankinformationConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'djmoney',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- SubmitData/models.py
+++ SubmitData/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 from django.contrib.auth.models import User
+from djmoney.models.fields import MoneyField
 from django.urls import reverse
 
 
@@ -10,10 +11,10 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     company_name = models.CharField(max_length=255, default="")
     account_number = models.CharField(max_length=255, default="")
-    statement_date = models.DateField(default=timezone.now)
+    statement_date = models.DateField(default=datetime.date.today)
     due_date = models.DateField(default=datetime.date.today)
-    date_entered = models.DateTimeField(default=timezone.now)
-    amount = models.CharField(max_length=255, default="")
+    date_entered = models.DateField(default=datetime.date.today)
+    amount = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
     is_paid = models.BooleanField(default=0)
     payment_method = models.CharField(max_length=50, default="")
     author = models.ForeignKey(User, on_delete=models.CASCADE) #this is where the rel. is. Foreign key is the user
