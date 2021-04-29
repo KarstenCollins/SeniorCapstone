9552840dfd32399e4aba07e450ebe86cfd6e4623
KarstenCollins <kbcollins17@mail.wou.edu>
2021-04-29 09:48:57 -0700

    Changes to income info model

--- IncomeInformation/models.py
+++ IncomeInformation/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from djmoney.models.fields import MoneyField
+import datetime
 
 # Create your models here.
 
 class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default="")
     source_name = models.CharField( max_length=100)
-    amount = models.CharField( max_length=20)
-    date_received = models.CharField( max_length=12)
+    amount = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
+    date_received = models.DateField(default=datetime.date.today)
+
 
     def __str__(self):
         return self.source_name
