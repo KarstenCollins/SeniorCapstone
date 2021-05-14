7b582b29f3e4d1914c9c1656c9abec0fb69aca9e
KarstenCollins <kbcollins17@mail.wou.edu>
2021-05-13 17:51:09 -0700

    random fixes

--- SubmitData/filters.py
+++ SubmitData/filters.py
@@ -1,6 +1,9 @@
 import django_filters
 from django_filters import DateFilter
+from django_filters.filters import DateFromToRangeFilter, DateRangeFilter
 from .models import Post
+from django import forms
+from django.forms import DateInput
 
 
 class BillFilter(django_filters.FilterSet):
@@ -11,8 +14,9 @@ class BillFilter(django_filters.FilterSet):
         }
 
 class SummariesFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name="due_date", lookup_expr='gte')
+    start_date = django_filters.DateFilter(field_name="due_date", lookup_expr='gte')
     end_date = DateFilter(field_name="due_date", lookup_expr='lte')
+    #due_date = DateRangeFilter(widget=django_filters.widgets.RangeWidget(attrs={'type':'datetime-local'}))
     class Meta:
         model = Post
         fields = ['due_date']
