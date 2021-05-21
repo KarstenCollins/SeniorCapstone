ff54708229d4a7bda491d48a15d76b3ac2fcb02e
KarstenCollins <kbcollins17@mail.wou.edu>
2021-05-20 17:09:49 -0700

    bank update and delete finished

--- BankInformation/views.py
+++ BankInformation/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import BankCreateForm
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.contrib import messages
 from .models import Bank
 
@@ -38,3 +38,40 @@ class BankListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self): #only show logged in users' data
         return self.model.objects.all().filter(user=self.request.user)
+
+class BankUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Bank
+    fields = ['bank_name', 'bank_acc_number', 'nickname', 'account_type', 'routing_number', 'notes']
+    context_object_name = 'banks'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user #sets author to logged in user
+        return super().form_valid(form)
+
+    def test_func(self):
+        bank = self.get_object()
+        if self.request.user == bank.user:
+            return True
+        return False
+
+
+class BankDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Bank
+    success_url = '/ViewBanks' #home page redirect when finished
+    #template_name = 'BankInformation/bank_del_update.html'
+    context_object_name = 'banks'
+
+    def test_func(self):
+        bank = self.get_object()
+        if self.request.user == bank.user:
+            return True
+        return False
+
+class BankDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Bank
+
+    def test_func(self):
+        bank = self.get_object()
+        if self.request.user == bank.user:
+            return True
+        return False
--- SubmitData/urls.py
+++ SubmitData/urls.py
@@ -14,22 +14,29 @@ from . import views as data_views
 from cal import views as cal_views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='Submit-Data'),  #Submit-Data is original
-        #'' means home
+    path('', PostListView.as_view(), name='Submit-Data'), 
+        
     path('post/<int:pk>/', PostDetailView.as_view(), name='data-detail'),
-                #go to primary key
     path('post/new/', PostCreateView.as_view(), name='create-data'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='update-data'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='delete-data'),
+
     path('ViewBanks/', bank_views.BankListView.as_view(), name='view-banks'),
+    path('bank/<int:pk>/delete/', bank_views.BankDeleteView.as_view(), name='delete-bank'),
+    path('bank/<int:pk>/update/', bank_views.BankUpdateView.as_view(), name='update-bank'),
     path('BankInfo/', bank_views.BankCreateView.as_view(), name='create-bank'),
-                                            #.as_view() if changed to class based
+    path('bank/<int:pk>/', bank_views.BankDetailView.as_view(), name='bank-detail'),
+                                            
+
     path('IncomeInfo/', income_views.IncomeCreateView.as_view(), name='create-income'),
     path('ViewIncome/', income_views.IncomeListView.as_view(), name='view-Income'),
+
     path('CompanyInfo/', company_views.CompanyCreateView.as_view(), name='create-company'),
     path('CompanyIncome/', company_views.CompanyListView.as_view(), name='view-company'),
+
     path('BillsNotPaid/', data_views.IsPaidView.as_view(), name='is-paid'),
     path('Summaries/', data_views.YearlyMonthlySummaryView.as_view(), name='view-summaries'),
+
     path(r'index/', cal_views.index, name='index'),
     path(r'calendar/', cal_views.CalendarView.as_view(), name='calendar'),
     path(r'event/new/', cal_views.event, name='event_new'),
