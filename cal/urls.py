24a1462a2e7fb85ef00650df2da503705352793d
mponce1411 <82016881+mponce1411@users.noreply.github.com>
2021-05-26 22:00:58 -0700

    Update urls.py
    test bug ##178147426

--- cal/urls.py
+++ cal/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
 	url(r'event/edit/(?P<event_id>\d+)/', views.event, name='event_edit'),
     #url(r'event/<int:pk>/remove', views.EventDeleteView.as_view(), name='remove_event'),
     url(r'listcalendar/', views.event_list_view, name='list-calendar'),
-    url(r'event/delete/<id>', views.event_delete_view, name='event-delete')
+    url(r'event/delete/<id>', views.event_delete_view, name='event-delete'),
+    url(r'return_home/', views.return_home, name='return_home')
 ]
